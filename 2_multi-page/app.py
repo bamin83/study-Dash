9080fc7d91572b73a29e9495cd2ae405dfd24c4a
tonakao <tonakao@jeol.co.jp>
2024-04-16 16:43:37 +0900

    ✨: マルチページ
    実施内容
    - 3つのページを作成
    - その内、一つはコールバック関数を含む
    
    補足
    初めに実行した際は、analysis-output/inputについてエラーが出たが、二回目以降は出なくなった。
    理由不明。

---
+++ 2_multi-page/app.py
@@ -0,0 +1,29 @@
+import dash
+from dash import Dash, html, dcc
+
+app = Dash(__name__, use_pages=True)
+
+app.layout = html.Div(
+    [
+        html.H1("Multi-page app with Dash Pages"),
+        html.Div(
+            [
+                html.Div(
+                    dcc.Link(
+                        f"{page['name']} - {page['path']}", href=page["relative_path"]
+                    )
+                )
+                for page in dash.page_registry.values()
+            ]
+        ),
+        dash.page_container,
+    ]
+)
+
+vals = [page for page in dash.page_registry.values()]
+print("vals: ")
+print(vals)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
